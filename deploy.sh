ff6b99dd11b712807b53c8a3f38531c0400f90a0
Connor Clark <cjamcl@gmail.com>
2019-04-30 00:03:10 -0700

    deply to hoten.cc

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -ex
 
-rm -rf deploy
-mkdir deploy
+yarn clean
+yarn build-client
 
-yarn build
-cp -r dist/index.html dist/world dist/gridia.js dist/gridia.map deploy
-
-cd deploy
-surge . gridia.surge.sh
-echo https://gridia.surge.sh/
+# TODO: make a site for this, don't use hoten.cc
+rsync -ahvz --delete ./dist/client/ root@hoten.cc:/var/www/hoten.cc/public_html/gridia
+echo https://hoten.cc/gridia/
