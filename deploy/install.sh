1269fb32a7fd2f03403ebe8fcd73102847510a77
Connor Clark <cjamcl@gmail.com>
2019-04-30 23:13:34 -0700

    comments, fix typoe

--- deploy/install.sh
+++ deploy/install.sh
@@ -8,7 +8,15 @@ yarn build
 rsync -ahvz --delete ./dist/client/ /var/www/hoten.cc/public_html/gridia
 
 # Server
-[[ ! -e /etc/systemd/system/gridia.service ]] ln -s /root/gridia/gridia-2019-wip/deploy/gridia.service /etc/systemd/system/gridia.service
+
+# Only make symlink the first time.
+[[ ! -e /etc/systemd/system/gridia.service ]] && ln -s /root/gridia/gridia-2019-wip/deploy/gridia.service /etc/systemd/system/gridia.service
+
+# Reload service config.
 systemctl daemon-reload
+
+# Register service to run on bootup.
 systemctl enable gridia
+
+# Restart.
 systemctl restart gridia
