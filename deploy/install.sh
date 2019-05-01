940bcb26329fe175f649a29d7b7cbc27291e7a34
Connor Clark <cjamcl@gmail.com>
2019-04-30 23:05:38 -0700

    install

--- deploy/install.sh
+++ deploy/install.sh
@@ -8,7 +8,7 @@ yarn build
 rsync -ahvz --delete ./dist/client/ /var/www/hoten.cc/public_html/gridia
 
 # Server
-ln -s /root/gridia/gridia-2019-wip/deploy/gridia.service /etc/systemd/system/gridia.service
+ln -s /root/gridia/gridia-2019-wip/deploy/gridia.service /etc/systemd/system/gridia.service || true
 systemctl daemon-reload
 systemctl enable gridia
 systemctl restart gridia
