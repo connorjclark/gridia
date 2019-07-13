a8d3b4a046fce2fc62b93c0db18cab021030e0f3
Connor Clark <cjamcl@gmail.com>
2019-07-13 16:35:06 -0700

    build for windows

---
+++ build/build-client.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const path = require("path");
+const ParcelBundler = require("parcel-bundler");
+
+function copyFileSync(source, target) {
+  var targetFile = target;
+
+  //if target is a directory a new file with the same name will be created
+  if (fs.existsSync(target)) {
+    if (fs.lstatSync(target).isDirectory()) {
+      targetFile = path.join(target, path.basename(source));
+    }
+  }
+
+  fs.writeFileSync(targetFile, fs.readFileSync(source));
+}
+
+function copyFolderRecursiveSync(source, target) {
+  var files = [];
+
+  //check if folder needs to be created or integrated
+  var targetFolder = path.join(target, path.basename(source));
+  if (!fs.existsSync(targetFolder)) {
+    fs.mkdirSync(targetFolder);
+  }
+
+  //copy
+  if (fs.lstatSync(source).isDirectory()) {
+    files = fs.readdirSync(source);
+    files.forEach(function (file) {
+      var curSource = path.join(source, file);
+      if (fs.lstatSync(curSource).isDirectory()) {
+        copyFolderRecursiveSync(curSource, targetFolder);
+      } else {
+        copyFileSync(curSource, targetFolder);
+      }
+    });
+  }
+}
+
+async function main() {
+  // Initializes a bundler using the entrypoint location and options provided
+  const bundler = new ParcelBundler("src/client/main.ts", {
+    outDir: "dist/client",
+    outFile: "gridia.js",
+    publicUrl: ".",
+    minify: false,
+  });
+
+  // Run the bundler, this returns the main bundle
+  // Use the events if you're using watch mode as this promise will only trigger once and not for every rebuild
+  await bundler.bundle();
+
+  copyFileSync("index.html", path.join("dist", "client"));
+  copyFolderRecursiveSync("world", path.join("dist", "client"));
+}
+
+main();
