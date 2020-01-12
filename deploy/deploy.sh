2907a196d7744458e660ddcba0e97d8738f061fb
Connor Clark <cjamcl@gmail.com>
2020-01-11 19:00:41 -0800

    fix server service

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,6 +4,7 @@ yarn clean
 yarn build-prod
 yarn test
 
+# TODO: rsync source code instead of using git on remote server.
 ssh root@hoten.cc 'cd gridia/gridia-2019-wip && git pull && ./deploy/install.sh'
 echo https://hoten.cc/gridia/
 
--- deploy/start.sh
+++ deploy/start.sh
@@ -2,6 +2,8 @@
 
 NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+NODE_VERSION=$(node -e 'console.log(require("./package.json").engines.node)')
+nvm use $NODE_VERSION
 
 node /root/gridia/gridia-2019-wip/dist/server/server/run.js \
   --ssl-cert /etc/letsencrypt/live/hoten.cc/fullchain.pem \
