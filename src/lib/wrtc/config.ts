13f28db4be04c402e19235b11cac1655c7d59740
Connor Clark <cjamcl@gmail.com>
2021-03-24 23:53:23 -0600

    forgot to add my turn/stun urls

--- src/lib/wrtc/config.ts
+++ src/lib/wrtc/config.ts
@@ -1,7 +1,10 @@
 export const WEBRTC_CONFIG: RTCConfiguration = {
   iceServers: [
     {
-      urls: 'stun:stun.l.google.com:19302',
+      urls: [
+        'stun:hoten.cc:3478',
+        'turn:hoten.cc:3478',
+      ],
       username: 'hoten',
       credentialType: 'password',
       credential: 'popgoestheweasel',
