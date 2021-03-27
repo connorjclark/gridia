b1c3852923eac0e3a744c09afe42069abead893c
Connor Clark <cjamcl@gmail.com>
2021-03-26 21:45:14 -0600

    tweak help menu formatting

--- src/client/ui/help-window.tsx
+++ src/client/ui/help-window.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { render, h, Component } from 'preact';
 import Game from '../game';
 
@@ -6,18 +7,16 @@ export function makeHelpWindow(game: Game) {
     render() {
       return <div class="help">
         <a href="https://github.com/connorjclark/gridia-2019-wip" target="_blank">GitHub</a>
-        <pre>
-          WASD to move character.
-          Number keys / Click to equip tool in inventory.
-          Arrow keys to select item in the world.
-          Space to use tool on selected item in the world. Alt to use hand.
-          To pick up item in the world: shift to pick up selected item, or drag and drop to inventory slot or character.
-        </pre>
+        <br></br>WASD to move character.
+        <br></br>Number keys / Click to equip tool in inventory.
+        <br></br>Arrow keys to select item in the world.
+        <br></br>Space to use tool on selected item in the world. Alt to use hand.
+        <br></br>To pick up item in the world: shift to pick up selected item, or drag and drop to inventory slot or character.
       </div>;
     }
   }
 
-  const el = game.makeUIWindow({name: 'help', cell: 'center'});
+  const el = game.makeUIWindow({ name: 'help', cell: 'center' });
   render(<HelpWindow />, el);
   return { el };
 }
