f61aa98806f6c3cd8ec6164531dad3199cb5052b
Connor Clark <cjamcl@gmail.com>
2021-05-08 20:57:24 -0600

    more help

--- src/client/ui/help-window.tsx
+++ src/client/ui/help-window.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+
 import { render, h, Component } from 'preact';
 import { useState } from 'preact/hooks';
 import Game from '../game';
@@ -6,26 +8,50 @@ import { makeUIWindow } from './ui-common';
 const sections: Record<string, string> = {
   'General': `
     <a href="https://github.com/connorjclark/gridia-2019-wip" target="_blank">GitHub</a>
-    <br>WASD to move character.
-    <br>Number keys / Click to equip tool in inventory.
-    <br>Arrow keys to select item in the world.
-    <br>Space to use tool on selected item in the world. Alt to use hand.
-    <br>To pick up item in the world: shift to pick up selected item, or drag and drop to inventory slot or character.
+
+    WASD: movement
+
+    Number keys / Click: Equip tool in inventory
+
+    Arrow keys / Click: select item in the world
+
+    Space: use tool on selected item in the world
+
+    Alt: use hand on selected item in the world
+
+    Shift: Pick up selected item (or drag/drop to slot or character)
   `,
   'Attributes, Skills and Leveling Up': `
-    Lorem Ipsum ...
+    Attribute levels consist of base points (selected when you create a character), and earned points (purchased with combat XP)
+    
+    Skill levels consist of base points (derived from attribute levels), and earned points (increase as you perform the skill)
+
+    Only XP from combat skills contribute to your Combat Level
+
+    With every Combat Level you gain a skill point, which can be used to learn new skills
   `,
   'Crafting': `
     Lorem Ipsum ...
   `,
   'Combat': `
+    Q/E to cycle nearby targets
+
+    R to attack currently selected target
+  `,
+  'Spells': `
     Lorem Ipsum ...
   `,
   'Chat': `
-    Lorem Ipsum ...
+    Type /help to list all chat commands
   `,
   'Land Ownership': `
-    Coming Soon
+    Land is broken up into 20x20 groups called sectors
+
+    Check "Show Grid" in Settings to visualize sectors
+
+    /landClaim to claim a sector
+    /landUnclaim to unclaim a sector
+    /landOwner to view who owns a sector
   `,
 };
 
@@ -44,7 +70,7 @@ export function makeHelpWindow(game: Game) {
         </div>
 
         <div class="current-section">
-          <div dangerouslySetInnerHTML={{ __html: sections[currentSection] }}></div>
+          <div dangerouslySetInnerHTML={{ __html: sections[currentSection].replace(/\n/g, '<br>') }}></div>
         </div>
       </div>;
     }
