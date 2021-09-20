05b6fca34c5f9528e425339c59a661e95ef22d1d
Connor Clark <cjamcl@gmail.com>
2021-09-19 21:44:45 -0700

    persist firebase auth

--- src/client/scenes/account-scene.ts
+++ src/client/scenes/account-scene.ts
@@ -10,54 +10,73 @@ import {SceneController} from './scene-controller';
 import {SelectCharacterScene} from './select-character-scene';
 
 export class AccountScene extends Scene {
-  private firebaseApp: any;
-  private firebaseAuth: any;
+  private firebaseApp = initializeApp({
+    apiKey: 'AIzaSyDQc_2k-ZcVnHxqTKp80OutU7y1LFp9zZA',
+    authDomain: 'gridia-434b8.firebaseapp.com',
+    projectId: 'gridia-434b8',
+    storageBucket: 'gridia-434b8.appspot.com',
+    messagingSenderId: '452601309463',
+    appId: '1:452601309463:web:be64956cba8171c1beef52',
+    measurementId: 'G-FB3PP93QG3',
+  });
+  private firebaseAuth = getAuth(this.firebaseApp);
 
   constructor(private controller: SceneController) {
     super(Helper.find('.account-scene'));
+  }
 
-    this.firebaseApp = initializeApp({
-      apiKey: 'AIzaSyDQc_2k-ZcVnHxqTKp80OutU7y1LFp9zZA',
-      authDomain: 'gridia-434b8.firebaseapp.com',
-      projectId: 'gridia-434b8',
-      storageBucket: 'gridia-434b8.appspot.com',
-      messagingSenderId: '452601309463',
-      appId: '1:452601309463:web:be64956cba8171c1beef52',
-      measurementId: 'G-FB3PP93QG3',
-    });
-    this.firebaseAuth = getAuth(this.firebaseApp);
+  onShow() {
+    super.onShow();
 
-    Helper.createChildOf(document.head, 'link', '', {
-      rel: 'stylesheet',
-      href: 'https://www.gstatic.com/firebasejs/ui/5.0.0/firebase-ui-auth.css',
-    });
-    const ui = new firebaseui.auth.AuthUI(this.firebaseAuth);
-    ui.start('.firebaseui-auth-container', {
-      signInOptions: [
-        GoogleAuthProvider.PROVIDER_ID,
-        {
-          provider: EmailAuthProvider.PROVIDER_ID,
-          requireDisplayName: false,
+    const unsubscribe = this.firebaseAuth.onAuthStateChanged(async (user) => {
+      unsubscribe();
+
+      if (user) {
+        const firebaseToken = await user.getIdToken(true);
+        await this.login(firebaseToken);
+        return;
+      }
+
+      Helper.createChildOf(document.head, 'link', '', {
+        rel: 'stylesheet',
+        href: 'https://www.gstatic.com/firebasejs/ui/5.0.0/firebase-ui-auth.css',
+      });
+      const ui = new firebaseui.auth.AuthUI(this.firebaseAuth);
+      ui.start('.firebaseui-auth-container', {
+        signInOptions: [
+          GoogleAuthProvider.PROVIDER_ID,
+          {
+            provider: EmailAuthProvider.PROVIDER_ID,
+            requireDisplayName: false,
+          },
+        ],
+        signInFlow: 'popup',
+        callbacks: {
+          signInSuccessWithAuthResult: (authResult) => {
+            this.onFirebaseAuthSuccess(authResult);
+
+            // Return control to the game (no redirect).
+            return false;
+          },
         },
-      ],
-      signInFlow: 'popup',
-      callbacks: {
-        signInSuccessWithAuthResult: (authResult) => {
-          this.onFirebaseAuthSuccess(authResult);
-          // Return control to the game (no redirect).
-          return false;
-        },
-      },
+      });
     });
   }
 
   async onFirebaseAuthSuccess(authResult: any) {
+    if (!this.firebaseAuth.currentUser) return;
+
     const firebaseToken = await this.firebaseAuth.currentUser.getIdToken(true);
     if (authResult.additionalUserInfo.isNewUser) {
       await this.controller.client.connection.sendCommand(CommandBuilder.registerAccount({
         firebaseToken,
       }));
     }
+
+    await this.login(firebaseToken);
+  }
+
+  async login(firebaseToken: string) {
     const response = await this.controller.client.connection.sendCommand(CommandBuilder.login({
       firebaseToken,
     }));
