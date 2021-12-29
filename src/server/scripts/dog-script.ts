a6da2eef3a9feb5d6669d3cae01a473bb1ed69ac
Connor Clark <cjamcl@gmail.com>
2021-12-29 14:37:52 -0800

    make your dog faster

--- src/server/scripts/dog-script.ts
+++ src/server/scripts/dog-script.ts
@@ -12,6 +12,7 @@ export class DogScript extends Script<{}> {
     const creature = this.server.createCreature({type: 96}, pos);
     if (!creature) return;
 
+    creature.speed = 0;
     // TODO: make server.tameCreature
     creature.tamedBy = playerConnection.player.id;
     this.server.creatureStates[creature.id].resetGoals();
