3b16d4dc7c144fd64a43bac8cdec8ec8f06f01f5
Connor Clark <cjamcl@gmail.com>
2022-01-18 00:52:24 -0800

    improve Bar styling

--- src/client/ui/components/bar.tsx
+++ src/client/ui/components/bar.tsx
@@ -4,7 +4,8 @@ export const Bar = (props: { label: string; color: string; current: number; max:
   const percent = 100 * props.current / props.max;
   return <div class="bar">
     <div class="bar__label">
-      <span>{props.label}:&nbsp;</span><span>{props.current}&nbsp;/&nbsp;{props.max}</span>
+      <span>{props.label}</span>
+      <span>{props.current}&nbsp;/&nbsp;{props.max}</span>
     </div>
     <div class="bar__bg" style={{width: `${percent}%`, backgroundColor: props.color}}>&nbsp;</div>
   </div>;
--- src/client/ui/windows/possible-usages-window.tsx
+++ src/client/ui/windows/possible-usages-window.tsx
@@ -3,7 +3,7 @@ import {render, h} from 'preact';
 import * as Content from '../../../content.js';
 import * as Utils from '../../../utils.js';
 import {UsageModule} from '../../modules/usage-module.js';
-import {ItemGraphic} from '..//components/graphic.js';
+import {ItemGraphic} from '../components/graphic.js';
 import {ComponentProps, createSubApp} from '../ui-common.js';
 
 interface State {
